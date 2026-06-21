6c658bb44803045dbbda21b59de13076b72a9759
AadilDiwan107 <aadildiwan107@gmail.com>
2026-06-21 14:47:00 +0530

    Refactor API to use login endpoint directly

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
-});
+const API = axios.post("http://localhost:5000/api/auth/login")
 
 export const loginUser = (data) => API.post("/login", data);
 export const signupUser = (data) => API.post("/register", data);
